ca3946d6641ec576af1abea072542eb8437e75dc
Ali hassnain <a71275643@gmail.com>
2026-07-29 12:30:18 +0500

    Add files via upload

---
+++ advance.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+# screenshot
+# pyautogui.sleep(3)
+# screen = pyautogui.screenshot()
+# screen.save("desktop.png")
+# alert,prompt,password
+
+# pyautogui.alert("Hello,My name is Ali Hassnain")
+
+# pyautogui.prompt("What is your name")
+# pyautogui.password("Please enter your password")
+
---
+++ keyboard_automation.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+# # write
+# # pyautogui.write("My name is Ali Hassnain")
+
+# # pyautogui.hotkey("win",'x')
+# pyautogui.hotkey("ctrl","a")
+# pyautogui.sleep(1)
+# pyautogui.hotkey("ctrl",'/')
+# keyboard keys
+print(pyautogui.KEY_NAMES)
